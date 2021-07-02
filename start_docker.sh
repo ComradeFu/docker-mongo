0dfaf324c4cc9999a54b6902cc177d1fd7529b36
ComradeFu <natsusao@gmail.com>
2021-07-02 15:32:58 +0800

    use -u

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,1 @@
-docker run -p 27017:27017 -d --name mongo -v $PWD/db:/data/db -v $PWD/dumps:/data/dumps --restart=always mongo
+docker run $(id -u):$(id -g) -p 27017:27017 -d --name mongo -v $PWD/db:/data/db -v $PWD/dumps:/data/dumps --restart=always mongo
